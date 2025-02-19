0cf854aea10b559cb8cc16b02d20bca31e7e7596
finxray <av.grigoryev@hotmail.com>
2025-02-19 22:57:26 +0300

    new ether

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -1,7 +1,8 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./ERC6909.sol"; // Assuming ERC6909 defines a fungible token standard
+import "./ERC6909.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PoolManager is ERC6909 {
@@ -20,51 +21,62 @@ contract PoolManager is ERC6909 {
         address asset1;
         uint poolID;
         Delta delta;
-        uint minAsset0Out; // Minimum asset0 user expects (for slippage protection)
-        uint minAsset1Out; // Minimum asset1 user expects (for slippage protection)
+        uint minOut; // Minimum amount user expects (for slippage protection)
     }
 
     mapping(uint => Pool) internal pools;
 
-    function swap(ExecutionParams[] memory params) external {
-        for (uint i = 0; i < params.length; i++) {
+    receive() external payable {} // Allows contract to receive ETH
+
+    function swap(ExecutionParams[] memory params) external payable {
+        uint length = params.length;
+        for (uint i = 0; i < length; i++) {
             ExecutionParams memory p = params[i];
             Pool storage pool = pools[p.poolID];
 
-            // Ensure pool has enough liquidity
-            require(p.delta.asset0 <= 0 || uint(p.delta.asset0) <= IERC20(p.asset0).balanceOf(msg.sender), "Insufficient asset0 balance");
-            require(p.delta.asset1 >= 0 || uint(-p.delta.asset1) <= pool.asset1Balance, "Insufficient asset1 balance");
+            uint newBalance0 = pool.asset0Balance;
+            uint newBalance1 = pool.asset1Balance;
 
-            uint amountOut0;
-            uint amountOut1;
-
-            // Handle asset0 transfer
-            if (p.delta.asset0 > 0) {
-                // User sends asset0 to pool
-                IERC20(p.asset0).transferFrom(msg.sender, address(this), uint(p.delta.asset0));
-                pool.asset0Balance += uint(p.delta.asset0);
-            } else if (p.delta.asset0 < 0) {
-                // Pool sends asset0 to user
-                amountOut0 = uint(-p.delta.asset0);
-                require(pool.asset0Balance >= amountOut0, "Pool: Insufficient asset0");
-                require(amountOut0 >= p.minAsset0Out, "Slippage exceeded on asset0");
-                pool.asset0Balance -= amountOut0;
-                IERC20(p.asset0).transfer(msg.sender, amountOut0);
+            if (p.delta.asset0 != 0) {
+                if (p.delta.asset0 > 0) {
+                    _transferOut(p.asset0, uint(p.delta.asset0), p.minOut);
+                    newBalance0 -= uint(p.delta.asset0);
+                } else {
+                    _transferIn(p.asset0, uint(-p.delta.asset0));
+                    newBalance0 += uint(-p.delta.asset0);
+                }
             }
 
-            // Handle asset1 transfer
-            if (p.delta.asset1 > 0) {
-                // User sends asset1 to pool
-                IERC20(p.asset1).transferFrom(msg.sender, address(this), uint(p.delta.asset1));
-                pool.asset1Balance += uint(p.delta.asset1);
-            } else if (p.delta.asset1 < 0) {
-                // Pool sends asset1 to user
-                amountOut1 = uint(-p.delta.asset1);
-                require(pool.asset1Balance >= amountOut1, "Pool: Insufficient asset1");
-                require(amountOut1 >= p.minAsset1Out, "Slippage exceeded on asset1");
-                pool.asset1Balance -= amountOut1;
-                IERC20(p.asset1).transfer(msg.sender, amountOut1);
+            if (p.delta.asset1 != 0) {
+                if (p.delta.asset1 > 0) {
+                    _transferOut(p.asset1, uint(p.delta.asset1), p.minOut);
+                    newBalance1 -= uint(p.delta.asset1);
+                } else {
+                    _transferIn(p.asset1, uint(-p.delta.asset1));
+                    newBalance1 += uint(-p.delta.asset1);
+                }
             }
+
+            // Store new balances
+            pool.asset0Balance = newBalance0;
+            pool.asset1Balance = newBalance1;
+        }
+    }
+
+    function _transferIn(address asset, uint amount) private {
+        if (asset == address(0)) {
+            require(msg.value == amount, "Incorrect ETH sent");
+        } else {
+            require(IERC20(asset).transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        }
+    }
+
+    function _transferOut(address asset, uint amount, uint minOut) private {
+        require(amount >= minOut, "Slippage exceeded");
+        if (asset == address(0)) {
+            payable(msg.sender).transfer(amount);
+        } else {
+            require(IERC20(asset).transfer(msg.sender, amount), "Transfer failed");
         }
     }
 }
