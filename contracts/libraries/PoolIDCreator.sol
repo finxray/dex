46f06ba270af9ffd78007a38a85e2a7408d10668
finxray <av.grigoryev@hotmail.com>
2025-07-20 15:45:27 +0400

    mission files for change of solidity version

--- contracts/libraries/PoolIDCreator.sol
+++ contracts/libraries/PoolIDCreator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.30;
 
 library PoolIDCreator {
     /// @notice Creates a pool ID as a uint256 for use with ERC6909.
--- contracts/libraries/PoolManagerLib.sol
+++ contracts/libraries/PoolManagerLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.30;
 
 import {Delta} from "../structs/Delta.sol";
 import {Inventory} from "../structs/Inventory.sol";
